b6edf7ac9d62a03111a7befc0a2ad048d60d1d59
osawa-koki <koki@osawa.dev>
2023-11-22 22:04:12 +0900

    不要なファイルの削除。

--- pages.ts
+++ pages.ts
@@ -9,16 +9,6 @@ const pages: Page[] = [
     emoji: '🏠',
     path: '/',
     name: 'Home'
-  },
-  {
-    emoji: '📖',
-    path: '/about/',
-    name: 'About'
-  },
-  {
-    emoji: '💓',
-    path: '/contact/',
-    name: 'Contact'
   }
 ]
 
