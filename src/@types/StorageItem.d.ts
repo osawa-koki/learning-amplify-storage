e0ffae7461a45b4e6ae42699b1b977ee16779d26
osawa-koki <koki@osawa.dev>
2023-11-23 18:42:53 +0900

    yarn lint --fix

--- src/@types/StorageItem.d.ts
+++ src/@types/StorageItem.d.ts
@@ -6,23 +6,23 @@ interface StorageItem {
   /**
    * Key of the object
    */
-  key: string;
+  key: string
   /**
    * Creation date of the object.
    */
-  lastModified?: Date;
+  lastModified?: Date
   /**
    * Size of the body in bytes.
    */
-  size?: number;
+  size?: number
   /**
    * An entity tag (ETag) is an opaque identifier assigned by a web server to a specific version of a resource found at
    * a URL.
    */
-  eTag?: string;
+  eTag?: string
   /**
    * The user-defined metadata for the object uploaded to S3.
    * @see https://docs.aws.amazon.com/AmazonS3/latest/userguide/UsingMetadata.html#UserMetadata
    */
-  metadata?: Record<string, string>;
+  metadata?: Record<string, string>
 };
